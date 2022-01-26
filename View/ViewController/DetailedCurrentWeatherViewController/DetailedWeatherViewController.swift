6e4a9652b7f500c85daca54ebc6f617c4ee848f5
samsamilsa <hyejeekim@me.com>
2022-01-26 19:08:35 +0900

    DetailedWeatherViewController 추가

---
+++ View/ViewController/DetailedCurrentWeatherViewController/DetailedWeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedWeatherViewController.swift
+//  How'sTheWeather
+//
+//  Created by HyeJee Kim on 2022/01/26.
+//
+
+import UIKit
+
+class DetailedWeatherViewController: UIViewController {
+    
+    // MARK: - Property
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
