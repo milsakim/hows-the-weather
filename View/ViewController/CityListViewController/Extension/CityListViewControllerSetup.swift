5c8a46b28cb70c59956e091061efe26d3f44c89f
samsamilsa <hyejeekim@me.com>
2022-01-27 15:08:52 +0900

    도시 목록 정렬 메뉴 액션 추가 및 navigation bar의 back button 수정

--- View/ViewController/CityListViewController/Extension/CityListViewControllerSetup.swift
+++ View/ViewController/CityListViewController/Extension/CityListViewControllerSetup.swift
@@ -10,7 +10,7 @@ import UIKit
 extension CityListViewController {
     
     func setupNavigation() {
-        let sortByCityName: UIAction = UIAction(title: "City Name") { action in
+        let sortByCityName: UIAction = UIAction(title: "City Name", state: .off) { action in
             if let viewModel = self.viewModel {
                 viewModel.supportingCities.sort {
                     return $0.name > $1.name
@@ -18,13 +18,23 @@ extension CityListViewController {
                 self.tableView.reloadData()
             }
         }
-        let sortByTemp: UIAction = UIAction(title: "Temperature") { action in
+        let sortByTemp: UIAction = UIAction(title: "Temperature", state: .off) { action in
             
         }
         let sortByDistance: UIAction = UIAction(title: "Distance") { action in
             
         }
-        let sortingMenu: UIMenu = UIMenu(title: "Sort by", children: [sortByCityName, sortByTemp, sortByDistance])
+        let sortingStandardMenu: UIMenu = UIMenu(title: "Sort by", options: [.singleSelection, .displayInline], children: [sortByCityName, sortByTemp, sortByDistance])
+        
+        let acsendingOrder: UIAction = UIAction(title: "Ascending") { action in
+        }
+        let descendingOrder: UIAction = UIAction(title: "Descending") { action in
+        }
+        let orderMenu: UIMenu = UIMenu(title: "In Order", options: [.singleSelection, .displayInline], children: [acsendingOrder, descendingOrder])
+        
+    
+        let sortingMenu: UIMenu = UIMenu(title: "Sort", options: [], children: [sortingStandardMenu, orderMenu])
+        
         let sortingButton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.up.arrow.down"), style: .plain, target: self, action: nil)
         sortingButton.menu = sortingMenu
         
@@ -32,16 +42,15 @@ extension CityListViewController {
         
         navigationItem.setRightBarButton(sortingButton, animated: false)
         navigationItem.setLeftBarButton(settingButton, animated: false)
+        
+        // back button title 삭제
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
     func setupTableView() {
         // table view의 data source와 delegate 설정
         tableView.dataSource = self
         tableView.delegate = self
-        
-        // table view의 header 설정
-//        tableView.tableHeaderView = headerView
-//        setupTableHeaderView()
         
         tableView.refreshControl = UIRefreshControl()
     }
