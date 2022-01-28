fb80b9c2d66f6e2218feca6928ad68efd86b252d
samsamilsa <hyejeekim@me.com>
2022-01-28 11:59:15 +0900

    - navigation item의 back 버튼의 타이틀 삭제

--- View/ViewController/DetailedCurrentWeatherViewController/DetailedWeatherViewController.swift
+++ View/ViewController/DetailedCurrentWeatherViewController/DetailedWeatherViewController.swift
@@ -20,15 +20,17 @@ class DetailedWeatherViewController: UIViewController {
     // MARK: - Deinitializer
     
     deinit {
-        iconImage = nil
+        print("--- DetailedWeatherViewController deinit ---")
     }
 
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
     }
     
     private func commonInit() {
+        setupNavigation()
         setupTableView()
     }
 
--- View/ViewController/DetailedCurrentWeatherViewController/Extension/DetailedWeatherViewControllerSetup.swift
+++ View/ViewController/DetailedCurrentWeatherViewController/Extension/DetailedWeatherViewControllerSetup.swift
@@ -9,6 +9,11 @@ import UIKit
 
 extension DetailedWeatherViewController {
     
+    func setupNavigation() {
+        // back button title 삭제
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+    
     func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
