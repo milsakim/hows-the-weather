c7005116d7254f033a78cfdfc70b52bcaf6b26d9
samsamilsa <hyejeekim@me.com>
2022-01-29 23:58:36 +0900

    - CityListViewController에 필요한 localization string의 key 추가

--- Common/LocalizationKey.swift
+++ Common/LocalizationKey.swift
@@ -9,6 +9,19 @@ import Foundation
 
 struct LocalizationKey {
     
+    // MARK: - CityListViewController
     
+    static let cityListViewControllerTitle: String = "Today's Weather"
+    
+    static let sort: String = "Sort"
+    static let cityName: String = "City Name"
+    static let temperature: String = "Temperature"
+    static let distance: String = "Distance"
+    static let ascending: String = "Ascending"
+    static let descending: String = "Descending"
+    
+    static let prefrences: String = "Prefrences"
+    static let celsicus: String = "Celsius"
+    static let fahrenheit: String = "Fahrenheit"
     
 }
