0298852c4a575a9a11cc6e3564af833dbd321b3a
samsamilsa <hyejeekim@me.com>
2022-01-29 23:35:12 +0900

    - 도시 정보 JSON을 읽어오기 위해 struct 프로퍼티 추가

---
+++ Common/LocalizationKey.swift
@@ -0,0 +1,14 @@
+//
+//  LocalizationKey.swift
+//  How'sTheWeather
+//
+//  Created by HyeJee Kim on 2022/01/29.
+//
+
+import Foundation
+
+struct LocalizationKey {
+    
+    
+    
+}
--- Model/City.swift
+++ Model/City.swift
@@ -14,6 +14,7 @@ struct SupportingCityList: Codable {
 struct City: Codable {
     let id: Int
     let name: String
+    let kr_name: String
     let state: String
     let country: String
     let coord: Coordinate
